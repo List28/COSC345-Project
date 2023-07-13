926b9f7103486f67e8d135b0aa0b25bbd05256ef
aaaansi <theshortrolow@gmail.com>
2023-07-13 19:33:35 +1200

    testing using vsc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World, just letting you know this works!" << std::endl;
     return 0;
 }
