2957cba39cfd0907889fa6bc610fc437ee60a142
aaaansi <theshortrolow@gmail.com>
2023-07-18 17:04:19 +1200

    Created a GUI base

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World, just letting you know this works!" << std::endl;
-    return 0;
-}
